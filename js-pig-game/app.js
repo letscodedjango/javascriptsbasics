6a2cd9b70451eb879aa0fe85763a9c5c31d3a4a9
choice-explorer <30986019+rsharma96@users.noreply.github.com>
2019-08-17 23:46:32 +0530

    Added code to index.html & app.js

--- js-pig-game/app.js
+++ js-pig-game/app.js
@@ -16,3 +16,72 @@ var finalScore = 0;
 
 var dice = Math.floor(Math.random()*6) + 1;
 console.log(dice);
+
+// Lets create a function that gonna hide the dice initially - before rolling dice
+// How to change the css styling using JS
+document.querySelector('.dice').style.display='none';
+
+// Now the dice will be visible on page when we hit the roll dice button
+// document.querySelector('.btn-roll').addEventListener('click', function(){
+//  document.querySelector('.dice').style.display='block'; // block is display value to display
+// })
+
+document.getElementById('current-1').textContent=0;
+document.querySelector('#current-0').textContent=0;
+document.getElementById('score-1').textContent=0;
+document.querySelector('#score-0').textContent=0;
+
+
+// Lets try to access all the elements value
+var score0 = document.querySelector('#score-0').textContent;
+console.log(score0);
+var score1 = document.getElementById('score-1').textContent;
+console.log(score1);
+var score1 = document.querySelector('#score-1').textContent;
+console.log(score1);
+// Lets change the value of score or we can say score-0 and score-1 elements with some random number
+
+// Now lets make some event handler and try to set some values to score-0 when we click on it
+document.querySelector('#score-0').addEventListener('click', function(){
+  var randNum = Math.floor(Math.random()*100);
+  console.log(randNum);
+  document.querySelector('#score-0').textContent = randNum;
+})
+
+// Now lets make changes to the score-1 simply when we hover on it
+document.getElementById('score-1').addEventListener('mouseover', function(){
+  var randNum = Math.floor(Math.random()*100);
+  console.log(randNum);
+  document.querySelector('#score-1').textContent = randNum;
+})
+
+// Lets implement new game button logic. So here when we click on new game buton, all the values should get reset to 0
+function reset(){
+  document.getElementById('current-1').textContent=0;
+  document.querySelector('#current-0').textContent=0;
+  document.getElementById('score-1').textContent=0;
+  document.querySelector('#score-0').textContent=0;
+  document.querySelector('.dice').style.display='none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', reset);
+
+// Now lets implement when user click on roll dice it should change at every click
+// so here simply we mean to change the image of dice at every click based on random number generated
+document.querySelector('.btn-roll').addEventListener('click', function(){
+ document.querySelector('.dice').style.display='block'; // block is display value to display
+ var dice = Math.floor(Math.random()*6) + 1;
+ document.querySelector('.dice').src='dice-' + dice + '.png';
+ scoreOne = scoreOne + dice;
+ document.getElementById('current-0').textContent=scoreOne;
+
+})
+
+// var randNum = Math.floor(Math.random()*100);
+// console.log(randNum);
+// document.querySelector('#score-0').textContent = randNum;
+
+// var randNum = Math.floor(Math.random()*100);
+// console.log(randNum);
+// document.getElementById('score-1').textContent= randNum;
+// score1 = randNum;
